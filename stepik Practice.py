93ad346d57bfaf59e851284ab4090ad26e24c298
Alisher Subanov <alisher.subanov@yandex.ru>
2023-06-20 21:11:10 +0500

    кортеж

--- stepik Practice.py
+++ stepik Practice.py
@@ -4,8 +4,8 @@
 # print(f'Привет меня зовут {first_name} {second_name}')
 # for i in range(1, 9):
 #     print(i)
-
-# Создаём пустой список и в него добавляем строки
+#
+# # Создаём пустой список и в него добавляем строки
 # new_list = []
 #
 #
@@ -21,21 +21,32 @@
 #
 # test()
 # print(new_list)
-
+#
 # get_split = "19.06.2023"
 # print(get_split.split("."))  # разделяем строку по "." на список ['19', '06', '2023']
 # print(get_split[-1])  # берём последний индекс списка 3
 # slice_1 = get_split.split(".")
 # print(slice_1[::2])  # slice[] start:stop:step
-# print(get_split[::2]) # slice
-# print(get_split[::-1]) # reverse
-# print(get_split[::-1][::-1]) # reverse reverse
-# print(get_split[::-1][::-1][::-1]) # reverse reverse reverse
-# print(get_split[::-1][::-1][::-1][::-1]) # reverse reverse reverse reverse
-# print(get_split[::-1][::-1][::-1][::-1][::-1]) # reverse reverse reverse reverse reverse
-# print(get_split[::-1][::-1][::-1][::-1][::-1][::-1]) # reverse reverse reverse reverse reverse reverse
+# print(get_split[::2])  # slice
+# print(get_split[::-1])  # reverse
+# print(get_split[::-1][::-1])  # reverse reverse
+# print(get_split[::-1][::-1][::-1])  # reverse reverse reverse
+# print(get_split[::-1][::-1][::-1][::-1])  # reverse reverse reverse reverse
+# print(get_split[::-1][::-1][::-1][::-1][::-1])  # reverse reverse reverse reverse reverse
+# print(get_split[::-1][::-1][::-1][::-1][::-1][::-1])  # reverse reverse reverse reverse reverse reverse
+#
+# list_a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# a, *b = list_a
+# print(a)
+# print(b)
 
-list_a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-a, *b = list_a
-print(a)
-print(b)
+# x = 10
+# while x >= 0:  # До тех пор пока x >= 0
+#     print(x)
+#     x -= 1  # x = x - 1
+# print("Всё!")
+
+
+t = (1, 2, 3, 4, 5, 6, 7, 8, 9)  # кортэж
+print(type(t))  # <class 'tuple'>
+print(*t, sep=":", end="!")  # => 1:2:3:4:5:6:7:8:9!
